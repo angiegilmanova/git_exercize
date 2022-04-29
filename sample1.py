9e2847fdac939f699467c896bf8a1b25eb39b81e
angiegilmanova <egilmanova@gmail.com>
2022-04-28 21:34:25 -0400

    typo fixed

--- sample1.py
+++ sample1.py
@@ -7,5 +7,5 @@ for i in range(10):
 
 print("New changes after last push, fixed PROJ-23455")
 
-print("this line isfor PROJ-23455 test failes")
-print("applied new changes to fix")
+print("this line is for PROJ-23455 test failed")
+print("applied new changes to fix the failed issue")
