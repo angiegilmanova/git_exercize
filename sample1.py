9869f90649e30031ada852560dc9e253c760626b
angiegilmanova <104595020+angiegilmanova@users.noreply.github.com>
2022-04-28 21:29:23 -0400

    angie fixed failed files

--- sample1.py
+++ sample1.py
@@ -6,3 +6,6 @@ for i in range(10):
     print(f"numbers : {i}")
 
 print("New changes after last push, fixed PROJ-23455")
+
+print("this line isfor PROJ-23455 test failes")
+print("applied new changes to fix")
